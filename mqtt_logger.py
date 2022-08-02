5144dc6739f84327160c71e66f7468985a31aeda
Xueman Mou <mou.xueman@gmail.com>
2022-08-02 17:04:58 +0800

    CTRL-C to safely quit; add batch script to run .py

--- mqtt_logger.py
+++ mqtt_logger.py
@@ -6,22 +6,24 @@ log specified mqtt messages
 author: Xueman Mou
 date: 2022/07/23
 version: 0.0.1
-modified: 2022/07/23 13:08:00 GMT +0800
+modified: 2022/08/02 16:05:00 GMT +0800
 
 reference: https://www.emqx.com/en/blog/how-to-use-mqtt-in-python
 developing environment: python 3.8.9
-dependencies: json, datetime, paha.mqtt, random
+dependencies: os, sys, traceback, json, datetime, paha.mqtt, random
 '''
 
+import os, sys, traceback
 import json
 from datetime import datetime
 from paho.mqtt import client as mqtt_client
 import random
 
 file_name = ""
+topics = {}
 
 def load_topic_file(file = 'topics.json'):
-	print('loading topic file %s ...' % file)
+	# print('loading topic file %s ...' % file)
 
 	fp = open(file, 'r')
 	obj = json.load(fp)
@@ -49,7 +51,8 @@ def start_logging():
 	
 	def on_connect(client, userdata, flags, rc):
 		if rc == 0:
-			print('Connected to MQTT Broker!')
+			print('正在录制数据……')
+			print('按组合键 CTRL + C 结束录制！')
 		else:
 			print('Failed to connect, return code %d\n', rc)
 
@@ -63,12 +66,19 @@ def start_logging():
 		topic = msg.topic
 		m_decode = str(msg.payload.decode("utf-8","ignore"))
 		message_handler(client, topic, m_decode)
-		print("message received: %s %s" % (topic, m_decode))
+		# print("message received: %s %s" % (topic, m_decode))
 
 	def message_handler(client, topic, msg):
-		fp.write('%s %s %s\n' % (get_current_timestamp(), topic, msg))
+		update = False
+		if topic not in topics:
+			update = True
+		elif topics[topic] != msg:
+			update = True
+		if update:
+			topics[topic] = msg
+			fp.write('%s %s %s\n' % (get_current_timestamp(), topic, msg))
 
-	fp = open(file_name, 'a+')
+	fp = open(file_name, 'a+', encoding='utf-8')
 	broker = "127.0.0.1"
 	port = 1883
 	client_id = f'python-mqtt-{random.randint(0, 1000)}'
@@ -83,8 +93,14 @@ def start_logging():
 	client.loop_forever()
 
 def main():
-	create_new_log_file()
-	start_logging()
-
+	try:
+		create_new_log_file()
+		start_logging()
+	except KeyboardInterrupt:
+		print('录制成功：%s' % os.path.abspath(file_name))
+	except Exception:
+		traceback.print_exc(file=sys.stdout)
+	sys.exit(0)
+			
 if __name__ == '__main__':
 	main()
---
+++ 录制数据.bat
@@ -0,0 +1,4 @@
+chcp 936
+
+pip install -r requirements.txt
+python mqtt_logger.py
